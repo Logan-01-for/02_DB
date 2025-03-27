cdd55b1ace63396ab6ba7f2a8981a390bee038f4
Logan-01-for <sjson0105@gmail.com>
2025-03-27 12:08:19 +0900

    0 3 2 7

---
+++ 재시험 평가.sql
@@ -0,0 +1,39 @@
+--[요구사항]
+--* 관리자 계정으로 접속하여 계정명 'test02', 비밀번호 '1234' 의 사용자 계정 생성
+--* test02 계정에 DB 접속 및 객체 생성 권한 부여
+
+--* 생성한 사용자 계정으로 접속하여 MEMBERS, BOARDS 테이블 생성
+-- - MEMBERS 테이블
+-- 1) MEMBER_ID 컬럼에 PRIMARY KEY 제약조건 설정.
+-- 2) MEMBER_PWD 컬럼에 NOT NULL 제약조건 설정.
+-- 3) MEMBER_EMAIL 컬럼에 MEM_EMAIL_UNQ 제약조건명을 가진 UNIQUE 제약조건 설정.
+
+-- BOARDS 테이블
+--1) BOARD_NO 컬럼에 PRIMARY KEY 제약조건 설정.
+--2) BOARD_WRITER 컬럼에 MEMERS 테이블의 MEMBER_ID를 참조하는 FOREIGN KEY 제약조건을 설정하되
+--   부모 테이블 데이터 삭제 시 참조하고 있는 자식 테이블의 데이터도 삭제하는 옵션 추가.
+--3) BOARD_REG_DATE(작성일) 컬럼에 기본값 SYSDATE 설정
+
+-- [작성한 SQL문]
+-- 사용자 계정 생성
+CREATE USER test02 IDENTIFIED BY 1234;
+GRANT CONNECT,RESOURCE TO test02;
+
+-- MEMBERS 테이블 생성
+CREATE TABLE MEMBERS(
+    MEMBER_ID VARCHAR2(20) PRIMARY KEY,
+    MEMBER_PWD VARCHAR2(20) NOT NULL,             
+    MEMBER_NAME VARCHAR2(30), 
+    MEMBER_AGE NUMBER,
+    MEMBER_EMAIL VARCHAR2(50),      
+    CONSTRAINT MEM_EMAIL_UNQ UNIQUE(MEMBER_EMAIL)
+);
+
+-- BOARDS 테이블 생성
+CREATE TABLE BOARDS(
+    BOARD_NO NUMBER PRIMARY KEY,
+    BOARD_TITLE VARCHAR2(100),
+    BOARD_CONTENT VARCHAR2(4000),
+    BOARD_WRITER VARCHAR2(20) REFERENCES MEMBERS(MEMBER_ID) ON DELETE CASCADE,
+    BOARD_REG_DATE DATE DEFAULT SYSDATE
+);​
