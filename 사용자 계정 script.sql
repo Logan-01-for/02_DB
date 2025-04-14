44685d4d771fcf7007918d15f90c9ef8f84f7544
Logan-01-for <sjson0105@gmail.com>
2025-04-14 17:37:25 +0900

    0 4 1 4

--- 사용자 계정 script.sql
+++ 사용자 계정 script.sql
@@ -14,13 +14,13 @@ ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 --DROP USER worbook;
 
 -- 계정 생성 구문 (username : kh / password : kh1234)
-CREATE USER workbook IDENTIFIED BY workbook;
+CREATE USER todo_jdbc IDENTIFIED BY todoList1234;
 
-GRANT RESOURCE, CONNECT TO workbook;
+GRANT RESOURCE, CONNECT TO todo_jdbc;
 
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
+ALTER USER todo_jdbc DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
